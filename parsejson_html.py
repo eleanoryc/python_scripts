d43987f6e91d139c67e3490bb4c2cf9eb3165810
Eleanor Cheung <echeung@salesforce.com>
2016-10-25 10:47:08 -0700

    update

--- parsejson_html.py
+++ parsejson_html.py
@@ -7,12 +7,14 @@
     @contact: echeung@salesforce.com
 
     TODO:  support multiple pods - done
-           generate HTML report - currently the output is display on the terminal
+           generate HTML report - currently the output is display on the terminal - in progress
            send report to email address
 """
 
 import argparse,sys
 import requests,json
+import time
+import datetime
 from pprint import pprint
 
 URL = 'https://imt.dmz.salesforce.com/api/v1/observers/reports/?provider='
@@ -26,15 +28,19 @@ def parsereport(rtype,p_list):
     if rtype == 'configchecker':
         report = 'config-checker'
         statuslist = ['ALERT']
+        # state, checkerName, message
         field = 'checkerName'
+        #field = ['checkerName','message']
     if rtype == 'prodtest':
         report = 'smoke-test'
         statuslist = ['failed','error','skipped']
+        # status, category, failure
         field = 'category'
+        #field = ['category','failure']
 
     for pod in p_list:
         content = ''
-        content = "<tr><td>Pod: </td><td>" + pod + "</td></tr>\n" + "<tr><td>" + report + ":\n</td>"
+        content = "<table border=1><tr><td bgcolor=#2ECCFA>Pod: </td><td bgcolor=#2ECCFA width=700>" + pod + "</td></tr>\n" + "<tr><td valign=top>" + report + ":\n</td>"
         print pod, report, ":"
 
         rURL = URL + report + '&instance=' + pod
@@ -44,23 +50,21 @@ def parsereport(rtype,p_list):
         if not data:
             print pod, ' has no report for ' + report + ' in IMT'
 #            exit(0)
-            content += "<td>" + pod + " has no report for " + report + " in IMT\n</td></tr>"
-            failtest = ''
+            failtest += pod + " has no report for " + report + " in IMT<br>"
             next
-        for json_data in data:
-#            pprint(json_data)
-            realdata = json_data['observations']
-            for k in realdata:
-                for status in statuslist:
-                    if realdata[k] == status:
-                        #print realdata[k], ': ' ,realdata[field]
-                        # create an variable and assign the content to it
-                        #failtest +=  "<td>something" + realdata[k] + ": " + realdata[field] + "</td></tr>\n"
-                        failtest +=  "something" + realdata[k] + ": " + realdata[field] + "<br>"
-        content += "<td>" + failtest + "</td></tr>\n"
+        else:
+            for json_data in data:
+#                pprint(json_data)
+                realdata = json_data['observations']
+                for k in realdata:
+                    for status in statuslist:
+                        if realdata[k] == status:
+                            failtest +=  realdata[k] + ": " + realdata[field]  + "<br>"
+            failtest += "<P>For detail of each fail tests, please check <br> URL: " + '<a href="' + rURL + '">here</a>'  + "</a></P>"
+        content += "<td>" + failtest + "</td></tr></table><br>\n"
+        failtest = ''
         print 'Content:'
         print content
-        #allcontent += content
         allcontent += content
     generatehtmlReport(allcontent)
 
@@ -72,15 +76,16 @@ def generatehtmlReport(htmlContent):
 # close the file when it's done
 # output to the screen the location and name of the file
     htmlHeader = "<html>\n<STYLE TYPE=\"text/css\">\n<!--\nTD{font-family: verdana; font-size: 10pt;}\n--->\n</STYLE>\n<body>"
-    htmlTableOpen = "<table border=1>"
-    htmlTableClose = "</table>"
-    htmlEndTag = "<br><br>\n</body>\n</html>"
-    htmlFileName = 'content.html'
+    #htmlTableOpen = "<table border=1>"
+    #htmlTableClose = "</table>"
+    htmlEndTag = "<br><br>\n</body>\n</html>" 
+    timestamp = time.strftime('%Y%m%d%H%M%S')
+    htmlFileName = 'content' + timestamp + '.html'
     with open(htmlFileName,'w') as htmlFileHandle: 
         htmlFileHandle.write(htmlHeader)
-        htmlFileHandle.write(htmlTableOpen)
+     #   htmlFileHandle.write(htmlTableOpen)
         htmlFileHandle.write(htmlContent)
-        htmlFileHandle.write(htmlTableClose)
+     #   htmlFileHandle.write(htmlTableClose)
         htmlFileHandle.write(htmlEndTag)
         htmlFileHandle.close()
 
@@ -93,6 +98,7 @@ def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--rtype', help="configchecker or prodtest")
     parser.add_argument('--pods', help="specify the instances (comma separated)")
+    parser.add_argument('--email', help="email address (comma separated)")
     args=parser.parse_args()
 
     if len(sys.argv) < 4:
